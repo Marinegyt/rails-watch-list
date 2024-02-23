bd7ee613f4e168293a60bd828894166e23f0b39b
Marine Gayet <marinegayet@gmail.com>
2024-02-23 13:14:40 +0100

    make front

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get "up" => "rails/health#show", as: :rails_health_check
-
+  root to: 'lists#index'
   resources :lists do
     resources :bookmarks, only: %i[index new create show]
   end
